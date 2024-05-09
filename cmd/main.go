7ccc5efba489ccc680603c0bdd8c56c1db839852
rachana_gupta <rachana.gupta@hpe.com>
2024-05-09 13:32:37 +0100

    Xml to spark and json to spark schema working

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"SchemaConversionTool/internal/domain"
 	"encoding/json"
+	_ "encoding/xml"
 	"fmt"
+	xj "github.com/basgys/goxml2json"
 	"html/template"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // SchemaResponse holds both the original JSON schema and the converted Spark schema
@@ -15,12 +18,12 @@ type SchemaResponse struct {
 	SparkSchema    interface{} `json:"spark_schema"`
 }
 
-func getJSONSchema(path string) ([]byte, error) {
-	var JSONSchema, err = os.ReadFile(path)
+func getSchemaFromPath(path string) ([]byte, error) {
+	var Schema, err = os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	return JSONSchema, nil
+	return Schema, nil
 }
 
 //var Schema = map[string]interface{}{
@@ -40,7 +43,8 @@ func getJSONSchema(path string) ([]byte, error) {
 
 func main() {
 
-	http.HandleFunc("/spark_schema", sparkSchemaHandler)
+	http.HandleFunc("/json_to_spark", jsonsparkSchemaHandler)
+	http.HandleFunc("/xml_to_spark", xmlSparkSchemaHandler)
 	http.Handle("/", http.FileServer(http.Dir("."))) // Serve static files (like the HTML file)
 
 	fmt.Println("Server running on http://localhost:8080")
@@ -49,8 +53,8 @@ func main() {
 }
 
 // Handler for Spark schema endpoint
-func sparkSchemaHandler(w http.ResponseWriter, r *http.Request) {
-	schemaBytes, err := getJSONSchema("inputSchemas/json_schema.json")
+func jsonsparkSchemaHandler(w http.ResponseWriter, r *http.Request) {
+	schemaBytes, err := getSchemaFromPath("inputSchemas/json_schema.json")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -71,7 +75,6 @@ func sparkSchemaHandler(w http.ResponseWriter, r *http.Request) {
 		OriginalSchema: schema,
 		SparkSchema:    sparkSchema,
 	}
-
 	//	// Marshal the schema to JSON with indentation
 	//	schemaJSON, err := json.MarshalIndent(response, "", "    ")
 	//	if err != nil {
@@ -142,4 +145,95 @@ func sparkSchemaHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+// Handler for Spark schema endpoint
+func xmlSparkSchemaHandler(w http.ResponseWriter, r *http.Request) {
+	originalSchema, schemaBytes := convertXMLToJSON()
+	var schema map[string]interface{}
+
+	// Unmarshal JSON data into the map
+	err := json.Unmarshal(schemaBytes, &schema)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// Simulated Spark schema data
+	sparkSchema, _ := domain.ConvertJSONSchemaToSparkSchema(schema["root"].(map[string]interface{}))
+	// Create a response struct
+	response := SchemaResponse{
+		OriginalSchema: string(originalSchema),
+		SparkSchema:    sparkSchema,
+	}
+	// Load HTML template
+	tmpl, err := template.New("schema").Funcs(template.FuncMap{
+		"toJSON": func(v interface{}) (string, error) {
+			jsonData, err := json.MarshalIndent(v, "", "    ")
+			if err != nil {
+				return "", err
+			}
+			return string(jsonData), nil
+		},
+	}).Parse(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schema Conversion</title>
+    <style>
+        .container {
+            display: flex;
+        }
+        .schema {
+            flex: 1;
+            margin: 10px;
+            border: 1px solid #ccc;
+            padding: 10px;
+            overflow: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="schema">
+            <h2>Original Schema</h2>
+            <pre>{{ .OriginalSchema }}</pre>
+        </div>
+        <div class="schema">
+            <h2>Spark Schema</h2>
+            <pre>{{ toJSON .SparkSchema }}</pre>
+        </div>
+    </div>
+</body>
+</html>
+`)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the response data
+	err = tmpl.Execute(w, response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func convertXMLToJSON() ([]byte, []byte) {
+
+	// Read XML content
+	xmlData, err := os.ReadFile("inputSchemas/xml_schema.xml")
+	if err != nil {
+		fmt.Println("Error reading XML file:", err)
+		return nil, nil
+	}
+	xml := strings.NewReader(string(xmlData))
+
+	jsonBytes, err := xj.Convert(xml)
+	if err != nil {
+		return xmlData, nil
+	}
+	return xmlData, jsonBytes.Bytes()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module SchemaConversionTool
 
 go 1.22.0
+
+require github.com/basgys/goxml2json v1.1.0
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
